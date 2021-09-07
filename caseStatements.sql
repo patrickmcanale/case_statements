29e5790aa126fe09bddb375825dfa91c1b5dc531
Pat Canale <73977982+patrickmcanale@users.noreply.github.com>
2021-09-07 10:49:35 -0500

    Add files via upload

---
+++ caseStatements.sql
@@ -0,0 +1,30 @@
+SELECT 
+	-- Select the date of the match
+	date,
+	-- Identify home wins, losses, or ties
+	CASE WHEN home_goal > away_goal THEN 'Home win!'
+         WHEN home_goal < away_goal THEN 'Home loss :(' 
+         ELSE 'Tie' END AS outcome
+FROM matches_spain;
+
+
+SELECT 
+	date,
+	-- Identify the home team as Barcelona or Real Madrid
+	CASE WHEN hometeam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END AS home,
+    -- Identify the away team as Barcelona or Real Madrid
+	CASE WHEN awayteam_id = 8634 THEN 'FC Barcelona' 
+         ELSE 'Real Madrid CF' END AS away
+FROM matches_spain
+WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
+      AND (awayteam_id = 8633 OR hometeam_id = 8633);
+
+
+	-- Select team_long_name and team_api_id from team
+SELECT
+	team_long_name,
+	team_api_id
+FROM teams_italy
+-- Filter by team long name
+WHERE team_long_name = 'Bologna';
